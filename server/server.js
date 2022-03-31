7de6ffb1ad87c84d0f2c7924972707bf1505daa6
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-30 20:00:36 -0700

    client

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./server/utils/auth');
+const { authMiddleware } = require('./utils/auth');
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-server-core");
+const path = require('path');
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./server/schemas');
-const db = require('./server/config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -35,6 +36,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
